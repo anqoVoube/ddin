052315926b9c31e4f2cbd3af429fda73ecd806be
Jamoliddin Bakhriddinov <jamoliddin.bakhriddinov@gmail.com>
2023-11-23 03:14:24 -0800

    Product request update

--- src/routes/product_request.rs
+++ src/routes/product_request.rs
@@ -1,129 +1,76 @@
-use axum::{http::StatusCode, response::IntoResponse, routing::post, Router, Extension, debug_handler};
+use axum::{http::StatusCode, response::IntoResponse, debug_handler};
 
 use axum_extra::extract::Multipart;
-use http::header;
-use axum_extra::extract::multipart::Field;
 
 use std::{collections::HashMap, str, fs::File, io::Write, path::Path, fs};
-use std::sync::Arc;
+use std::sync::Mutex;
 use axum::response::Response;
-use sea_orm::DatabaseConnection;
-use reqwest::multipart;
-use bytes::Bytes;
+use uuid::Uuid;
+use once_cell::sync::Lazy;
 
-// (title, code, file as field)
+static GLOBAL_DATA: Lazy<Mutex<HashMap<i32, String>>> = Lazy::new(|| {
+    let mut m = HashMap::new();
+    m.insert(13, "Spica".to_string());
+    m.insert(74, "Hoyten".to_string());
+    Mutex::new(m)
+});
+
+
+#[derive(Debug, Default)]
 pub struct ObjectBody{
     title: Option<String>,
     code: Option<String>,
-    file: Option<Vec<u8>>
+    file: Option<String>
 }
 
 #[debug_handler]
-pub async fn upload(
-    Extension(database): Extension<DatabaseConnection>,
-    mut inc_multipart: Multipart
+async fn upload(
+    mut multipart: Multipart
 ) -> Response {
-    let mut data: HashMap<usize, ObjectBody> = HashMap::new();
-    let mut headers = header::HeaderMap::new();
-    headers.insert("x-api-key", header::HeaderValue::from_static("jamoliddin"));
-    let mut fields: Vec<Field> = vec!();
-    while let Some(field) = inc_multipart.next_field().await.unwrap(){
-        fields.push(field)
-    }
-    for field in fields{
+    let mut data = HashMap::new();
+    let mut objects: HashMap<usize, ObjectBody> = HashMap::new();
+    let mut file_names: HashMap<String, String> = HashMap::new();
+    while let Some(field) = multipart.next_field().await.unwrap() {
         let name = field.name().unwrap().to_string();
         let count = name.split("_").collect::<Vec<&str>>().first().unwrap().parse::<usize>().unwrap();
-        data.entry(count).or_insert(
-            ObjectBody{
-                title: None,
-                code: None,
-                file: None
-            }
-        );
+        let mut object = objects.entry(count).or_insert(ObjectBody{..Default::default()});
 
         if name.ends_with("photo") {
             // Generate a unique filename for the image
-            let object = data.get_mut(&count).unwrap();
-            object.file = Some(field.bytes().await.unwrap().to_vec());
-        }
-        else if name.ends_with("title"){
-            let object = data.get_mut(&count).unwrap();
-            object.title = Some(field.text().await.unwrap());
-        }
+            let filename = format!("{}.jpg", object.title.unwrap());
 
-        else {
-            // Handle text data
-            let object = data.get_mut(&count).unwrap();
-            object.code = Some(field.text().await.unwrap());
+            if let Ok(_) = fs::create_dir_all("/some/dir"){
+                println!("Created directory")
+            }
+            // Specify the directory where the file will be saved
+            let filepath = Path::new("").join(filename.clone());
+
+            // Get the image data
+            let file_data = field.bytes().await.unwrap();
+
+            // Save the file
+            let mut file = File::create(filepath).unwrap();
+            file.write_all(&file_data).unwrap();
+            object.file = Some(filename);
+
+        } else  {
+            let bytes = field.bytes().await.unwrap();
+            let text_data: String = str::from_utf8(&bytes).unwrap().to_string();
+            if name.ends_with("title"){
+                object.title = Some(text_data);
+            } else {
+                object.code = Some(text_data);
+            }
         }
     }
-    let keys: Vec<_> = data.keys().cloned().collect();
-    for key in keys{
-        let object: ObjectBody = data.remove(&key).unwrap();
-        let filename = format!("{}.jpg", to_filename(&object.title.clone().unwrap()));
-        let title: &str = &object.title.clone().unwrap();
-        let code: &str = &object.code.clone().unwrap();
-        // Get the image data
-
-        // // Save the file
-        // let mut file = File::create(filepath).unwrap();
-        // file.write_all(&file_data).unwrap();
-
-        //
-        // let file_part = reqwest::multipart::Part::bytes(file)
-        //     .file_name(filepath.as_path())
-        //     .mime_str("image/jpg")
-        //     .unwrap();
-
-        let part = multipart::Part::bytes(object.file.unwrap()).file_name(filename);
-        let form = multipart::Form::new()
-            .text("templateId", "value1")
-            .part("imageFile", part);
-
-
-        // "https://beta-sdk.photoroom.com/v1/render"
-        // Send the request
-        let response = reqwest::Client::new().post("http://127.0.0.1:8000/")
-            .multipart(form)
-            .send()
-            .await.unwrap();
-    }
-
-
-
-    // let filename = format!("{}-{}.jpg", Uuid::new_v4(), name);
-
-    // // Specify the directory where the file will be saved
-    // let filepath = Path::new("").join(filename);
-
-    // // Get the image data
-    // let file_data = field.bytes().await.unwrap();
-
-    // // Save the file
-    // let mut file = File::create(filepath).unwrap();
-    // file.write_all(&file_data).unwrap();
 
     (StatusCode::OK).into_response()
 }
 
-
-fn to_filename(s: &str) -> String {
-    let mut result = String::new();
-    let mut prev_was_digit = false;
-
-    for c in s.to_lowercase().chars() {
-        match c {
-            'g' if prev_was_digit => {}, // Skip 'g' if it follows a digit
-            c if c.is_alphanumeric() || c == ' ' || c == '-' => {
-                result.push(c);
-                prev_was_digit = c.is_digit(10);
-            },
-            _ => prev_was_digit = false,
-        }
-    }
-
-    result.replace(" ", "-")             // Replace spaces with dashes
-        .split('(').next().unwrap()    // Split at '(' and take the first part
-        .trim_end_matches('-')         // Trim trailing dashes if any
-        .to_string()
+pub fn process_title(title: &str) -> String{
+    let split_vec = title.split(" (").take(2).collect::<Vec<&str>>();
+    let [name, weight] = <[&str; 2]>::try_from(split_vec).ok().unwrap();
+    let replaced = name.replace(" ", "-").to_lowercase();
+    let replaced_weight = weight.replace(")", "");
+    format!("{}-{}.jpg", replaced, replaced_weight)
 }
