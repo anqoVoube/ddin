989fde6cbf524d236e94a3b93759e476ea901836
JAMOLIDDIN BAKHRIDDINOV <milsolve@gmail.com>
2023-12-08 22:40:26 +0500

    Removing auth from find

--- src/routes/find/sell.rs
+++ src/routes/find/sell.rs
@@ -18,20 +18,21 @@ use crate::routes::find::{find_weight_item, find_no_code_product, find_product, 
 
 
 pub async fn search(
-    Extension(auth): Extension<Auth>,
+    // Extension(Auth{ user_id, business_id }): Extension<Auth>,
     Extension(database): Extension<DatabaseConnection>,
     Query(query): Query<Search>
 ) -> Response{
         println!("{} {:?}", query.search, query.r#type);
+        let business_id: i32 = 1;
         match query.r#type{
             Types::Product => {
-                let data = find_product(query.search, auth.business_id, &database).await;
+                let data = find_product(query.search, business_id, &database).await;
                 ().into_response()
             },
             Types::WeightItem => {
                 let data = find_weight_item(
                     query.search,
-                    auth.business_id,
+                    business_id,
                     &database
                 ).await;
                 (
@@ -42,7 +43,7 @@ pub async fn search(
             Types::NoCodeProduct => {
                 let data = find_no_code_product(
                     query.search,
-                    auth.business_id,
+                    business_id,
                     &database
                 ).await;
                 (
