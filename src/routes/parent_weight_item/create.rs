ca0863346ad9199b4e7aab2fc2e9f774fe4ba4b2
JAMOLIDDIN BAKHRIDDINOV <milsolve@gmail.com>
2023-12-13 21:00:17 +0500

    Add Unique Product functionality

--- src/routes/parent_weight_item/create.rs
+++ src/routes/parent_weight_item/create.rs
@@ -12,15 +12,21 @@ use crate::database::prelude::ParentWeightItem;
 use crate::routes::utils::{default_created, internal_server_error, hash_helper::generate_uuid4, space_upload::upload_to_space, bad_request};
 use sea_orm::QueryFilter;
 use sea_orm::ColumnTrait;
+use crate::core::auth::middleware::Auth;
+
+const DEFAULT_EXPIRATION_IN_DAYS: i32 = 365;
+
 pub struct RequestBody{
     main_image: Option<String>,
     title: Option<String>,
     description: Option<String>,
-    images: Vec<String>
+    images: Vec<String>,
+    expiration_in_days: Option<i32>,
 }
 
 #[debug_handler]
 pub async fn upload(
+    Extension(Auth{user_id, business_id}): Extension<Auth>,
     Extension(database): Extension<DatabaseConnection>,
     mut multipart: Multipart
 ) -> Response {
@@ -28,17 +34,18 @@ pub async fn upload(
         main_image: None,
         title: None,
         description: None,
-        images: vec!()
+        images: vec!(),
+        expiration_in_days: None
     };
     while let Some(field) = multipart.next_field().await.unwrap() {
         let name = field.name().unwrap().to_string();
 
         if name.ends_with("images") {
             let file_data: Vec<u8> = field.bytes().await.unwrap().to_vec();
-            let unique_hash = generate_uuid4();
-            match upload_to_space(file_data, unique_hash.clone()).await{
+            let file_name = format!("{}.jpg", generate_uuid4());
+            match upload_to_space(file_data, file_name.clone()).await{
                 Ok(_) => {
-                    request_body.images.push(unique_hash);
+                    request_body.images.push(file_name);
                 },
                 Err(err) => {
                     error!("Error: {:?}", err);
@@ -46,35 +53,44 @@ pub async fn upload(
                 }
             }
 
-        } else if name.ends_with("title") {
-            let bytes = field.bytes().await.unwrap();
-            let text_data: String = str::from_utf8(&bytes).unwrap().to_string();
-            // checking for title existence
-            match ParentWeightItem::find()
-                .filter(parent_weight_item::Column::Title.eq(text_data.clone()))
-                .one(&database).await{
-                Ok(Some(_)) => {
-                    return bad_request("Title already exists");
-                },
-                Ok(None) => {
-                    request_body.title = Some(text_data);
+        } else if name.ends_with("main_image") {
+            let file_data: Vec<u8> = field.bytes().await.unwrap().to_vec();
+            let file_name = format!("{}.jpg", generate_uuid4());
+            match upload_to_space(file_data, file_name.clone()).await{
+                Ok(_) => {
+                    request_body.main_image = Some(file_name);
                 },
                 Err(err) => {
+                    println!("{}", err);
                     error!("Error: {:?}", err);
                     return internal_server_error();
                 }
             }
         } else {
-            let file_data: Vec<u8> = field.bytes().await.unwrap().to_vec();
-            let unique_hash = generate_uuid4();
-            match upload_to_space(file_data, unique_hash.clone()).await{
-                Ok(_) => {
-                    request_body.main_image = Some(unique_hash);
-                },
-                Err(err) => {
-                    error!("Error: {:?}", err);
-                    return internal_server_error();
+            let bytes = field.bytes().await.unwrap();
+            let text_data: String = str::from_utf8(&bytes).unwrap().to_string();
+            // checking for title existence
+            if name.ends_with("title"){
+                match ParentWeightItem::find()
+                    .filter(parent_weight_item::Column::Title.eq(text_data.clone()))
+                    .one(&database).await{
+                    Ok(Some(_)) => {
+                        return bad_request("Title already exists");
+                    },
+                    Ok(None) => {
+                        request_body.title = Some(text_data);
+                    },
+                    Err(err) => {
+                        error!("Error: {:?}", err);
+                        return internal_server_error();
+                    }
                 }
+            } else if name.ends_with("description"){
+                request_body.description = Some(text_data);
+            } else if name.ends_with("expiration"){
+                request_body.expiration_in_days = Some(text_data.parse::<i32>().unwrap());
+            } else {
+                return bad_request("Invalid field name");
             }
         }
     }
@@ -84,6 +100,8 @@ pub async fn upload(
         description: Set(request_body.description.clone().unwrap()),
         main_image: Set(request_body.main_image.clone()),
         images: Set(request_body.images.clone()),
+        expiration_in_days: Set(request_body.expiration_in_days.unwrap_or(DEFAULT_EXPIRATION_IN_DAYS)),
+        business_id: Set(Some(business_id)),
         ..Default::default()
     };
 
