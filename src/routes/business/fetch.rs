ffa8c7f818b78530ec42cace0c0ee59e2648bdd1
JAMOLIDDIN BAKHRIDDINOV <milsolve@gmail.com>
2023-12-31 12:24:25 +0500

    Cookie update

--- src/routes/business/fetch.rs
+++ src/routes/business/fetch.rs
@@ -46,7 +46,6 @@ impl From<BusinessModel> for BusinessSchema {
 pub async fn list(
     // Extension(Auth{user_id}): Extension<Auth>,
     Extension(database): Extension<DatabaseConnection>,
-    Path(count): Path<i32>,
 ) -> Result<Json<BusinessesSchema>, StatusCode> {
     let user_id = 94;
     let businesses = Business::find()
