d6c61f1cd8b55f85c9a27d10d6cd9bcef5fdabed
Jamoliddin Bakhriddinov <jamoliddin.bakhriddinov@gmail.com>
2024-01-09 03:15:06 -0800

    Postgres' integration with bot

--- src/bot/mod.rs
+++ src/bot/mod.rs
@@ -1,11 +1,14 @@
 use dotenvy_macro::dotenv;
+use sea_orm::{Condition, EntityTrait};
 use teloxide::Bot;
 use teloxide::dispatching::dialogue::InMemStorage;
 use teloxide::payloads::SendMessageSetters;
 use teloxide::prelude::{Dialogue, Message, Requester};
 use teloxide::types::{ButtonRequest, KeyboardButton, KeyboardMarkup};
 use teloxide::update_listeners::webhooks::Options;
-
+use crate::database::prelude::User;
+use crate::database::user;
+use crate::POSTGRES_CONNECTION;
 
 
 type MyDialogue = Dialogue<State, InMemStorage<State>>;
@@ -40,6 +43,13 @@ pub async fn receive_full_name(bot: Bot, dialogue: MyDialogue, msg: Message) -> 
     match msg.contact() {
         Some(contact) => {
             println!("{}", contact.phone_number);
+            let mut condition = Condition::all();
+            condition = condition.add(user::Column::PhoneNumber.eq(contact.phone_number.clone()));
+            match User::find().filter(condition).one(&POSTGRES_CONNECTION.get().unwrap()).await{
+                Ok(Some(_)) => println!("Found"),
+                Ok(None) => println!("Not found"),
+                Err(e) => println!("Error, {:?}", e)
+            }
             bot.send_message(msg.chat.id, "How old are you?").await?;
             dialogue.exit().await.unwrap();
         }
